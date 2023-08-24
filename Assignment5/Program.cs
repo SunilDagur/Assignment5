d1c59859e5f0401345263c2ec0891ab8f3010c28
Sunil Dagur <sunildagur566@gmail.com>
2023-08-24 21:56:33 +0530

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,37 @@
+﻿// File: PersonProperties.cs
+
+using System;
+
+public partial class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+// File: PersonMethods.cs
+
+
+public partial class Person
+{
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
+
+
+// File: Program.cs
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            FirstName = "Sunil",
+            LastName = "Dagur"
+        };
+
+        person.PrintFullName();
+    }
+}
